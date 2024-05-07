1a6dd2ecb82404d1bbeceac4e7161c269c6c66a5
splinterwow <asosiymenu007@gmail.com>
2024-05-07 16:04:04 +0500

    vazifa bajarildi

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,16 +5,25 @@ function Cart() {
   const { data } = useContext(GlobalContext);
   console.log(data);
 
+  if (!data) {
+    return <div className="text-center text-2xl"><b>Loading...</b></div>; 
+  }
+
   return (
-    <div className="align-content">
-      <div>Cart</div>
+    <div className="align-content  space-x-3">
+      <h1 className="text-center font-bold text-3xl">All products</h1>
       <ul>
         {data.map((item) => (
           <li key={item.id}>
-            <img src={item.thumbnail} alt={item.description} />
-            <p>{item.description}</p>
-            <p>Price: {item.price}</p>
-            <p>Stock: {item.stock}</p>
+            <div className="flex items-center border border-solid border-black mt-3"> 
+              <img className="mb-3 w-45  h-45  object-cover border-r-2  border-orange-300" src={item.thumbnail} alt={item.description} />
+              <div className="p-5">
+                <p className=" -mt-36 mb-3"><b>Description: </b>{item.description}</p>
+                <p className="mb-3"><b>Price: </b>{item.price}</p>
+                <p className="mb-3"><b>Stock: </b>{item.stock}</p>
+                <button className="btn btn-primary ">Buy</button>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
